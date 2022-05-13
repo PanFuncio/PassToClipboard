212426be636886d7026c246dbf08169a5ca197c1
PanFuncio <57959560+PanFuncio@users.noreply.github.com>
2022-05-13 22:34:48 +0200

    Update Źródło.cpp
    Login function doesnt exit you out of the app now

--- PassToClipGithub/Źródło.cpp
+++ PassToClipGithub/Źródło.cpp
@@ -71,10 +71,9 @@ public:
     }
     virtual void InitiliazePasswordMap() // these are your passwords, add new ones as you wish
     {
-        passwords2.insert({ "test1","pass1" });
-        passwords2.insert({ "test2","pass2" });
-        passwords2.insert({ "test3","trshr53453tht101rystr36363gfsd" });
-        passwords2.insert({ "boink!234&w","333style_hero_of_my_story333" });
+        //passwords2.insert({ "x","y" });
+        
+
     }
     virtual void ListPasswords()
     {
@@ -110,52 +109,58 @@ int main()
     string task;
     int task2;
     cout << "please login with your set password " << endl;
-    string ActualPassword = "123"; // set this to your preferred password to get into the program
-    string YourPassword = takePasswdFromUser();
+    string ActualPassword = "Password"; // set this to your preferred password to get into the program
+    
+    bool login_successful = false;
     //cin >> YourPassword;
-    if (YourPassword == ActualPassword)
+    while(!login_successful)
     {
-        "Login succesful welcome";
-    }
-    else
-    {
-        cout << "ouch";
-        return 0;
+        string YourPassword = takePasswdFromUser();
+        if (YourPassword == ActualPassword)
+        {
+            cout << "Login succesful welcome" << endl << endl;
+            login_successful = true;
+        }
+        else
+        {
+            cout << "Nope, try again" <<endl <<endl;
+            //cin >> takePasswdFromUser();
+        }
     }
     string keyword;
     ToNum passwords;
-    do
-    {
-        cout << "Type the password KEYWORD you want" << endl << "type LIST to get a list of passwords you can access" << endl << "type EXIT to close the program" << endl;
-        passwords.InitiliazePasswordMap();
-        cin >> task;
-
-        task2 = Action(task);
-
-        switch (task2)
+    if(login_successful)
+    { 
+        do
         {
-            case 0:
-            {
-                exit = 1;
-                cout << "exiting..." << endl;
-                break;
-            }
-            case 99:
-            {
-                passwords.ListPasswords();
-                break;
-            }
-            case 1:
-            {
-                keyword = task;
-                passwords.getPassword(keyword);
-                break;
-            }
- 
-        }
-    } while (exit == 0);
+            cout << "Type the password KEYWORD you want" << endl << "type LIST to get a list of passwords you can access" << endl << "type EXIT to close the program" << endl;
+            passwords.InitiliazePasswordMap();
+            cin >> task;
 
+            task2 = Action(task);
+
+            switch (task2)
+            {
+                case 0:
+                {
+                    exit = 1;
+                    cout << "exiting..." << endl;
+                    break;
+                }
+                case 99:
+                {
+                    passwords.ListPasswords();
+                    break;
+                }
+                case 1:
+                {
+                    keyword = task;
+                    passwords.getPassword(keyword);
+                    break;
+                }
+ 
+            }
+        } while (exit == 0);
+    }
     return 0;
 }
-
-
